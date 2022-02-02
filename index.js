227638f2598bc00f67f795e972d95eaa6a8ae99a
Thulunga <thulunga.tb@gmail.com>
2022-02-02 11:57:13 +0530

    db connection added

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 
 let port = process.env.PORT || 3000;
+
+mongoose.connect(
+    'mongodb+srv://Thulunga:Tbasum398@cluster0.szsdt.mongodb.net/dokhona?retryWrites=true&w=majority', { useNewUrlParser: true }
+    ).then(() => {
+        console.log('Connected to database');
+    }).catch(err => {
+        console.log(err);
+    });
 
 app.get('/', (req, res) => {
     res.send('Hello World Thulunga Basumatary what are you doing!');
