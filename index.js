59e1318077aa50fb7a9c74ccb37cbf1208b0a7b4
Thulunga <thulunga.tb@gmail.com>
2022-02-02 11:21:01 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+let port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World Thulunga Basumatary!');
+})
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
+
+
