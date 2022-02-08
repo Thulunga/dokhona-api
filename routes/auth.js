5f624012752f2a9bbcaa41fdfc37d5156fd716dc
Thulunga <thulunga.tb@gmail.com>
2022-02-08 12:50:13 +0530

    corrected

--- routes/auth.js
+++ routes/auth.js
@@ -1,55 +1,54 @@
-const router = require('express').Router();
-const User = require('../models/User');
+const router = require("express").Router();
+const User = require("../models/User");
 const CryptoJS = require("crypto-js");
 
 //Register
-router.post('/register', async (req, res) => {
-    const newUser = new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: CryptoJS.AES.encrypt(
-            req.body.password,
-            process.env.PASS_SEC
-        ).toString(),
-    });
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SEC
+    ).toString(),
+  });
 
-    try {
-        const savedUser = await newUser.save();
-        res.status(201).json(savedUser);
-      } catch (err) {
-        res.status(500).json(err);
-      }
-
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 //Login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({
+      username: req.body.username,
+    });
 
-router.post("/login", async (req,res) => {
-    try {
-        const user = await User.findOne(
-            {
-                username: req.body.username
-            }
-        );
+    if (!user) {
+      res.status(401).json("Wrong User Name");
+    } else {
+      const hashedPassword = CryptoJS.AES.decrypt(
+        user.password,
+        process.env.PASS_SEC
+      );
 
-        !user ? res.status(401).json({ message: "User not found" }) : null;
+      const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+      const inputPassword = req.body.password;
 
-        const hashedPassword = CryptoJS.AES.decrypt(
-            user.password,
-            process.env.PASS_SEC
-        );
-
-        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
-
-        const inputPassword = req.body.password;
-
-        originalPassword !== inputPassword ? res.status(401).json({ message: "Password incorrect" }) : null;
-
-    } catch (error) {
-        res.status(500).json(error);
-        
+      if (inputPassword !== originalPassword) {
+        res.status(401).json("Wrong Password");
+      } else {
+        const {password, ...others} = user._doc;
+        res.status(200).json(others);
+      }
     }
-})
-
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
