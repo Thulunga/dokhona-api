2cfbc86ed4a45d3d64b78d7b8a1eade707830756
Thulunga <thulunga.tb@gmail.com>
2022-02-15 21:39:46 +0530

    commit

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const cors = require("cors");
 dotenv.config();
 
 
-let port = process.env.PORT || 3000;
+let port = process.env.PORT || 5000;
 
 mongoose.connect(
     process.env.MONGO_URL, { useNewUrlParser: true }
